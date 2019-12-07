7b8ba394055f626b9f5359bd5fe48107fcf25403
Nicholas W.M. Ritchie <nicholas.wm.ritchie@gmail.com>
2019-12-07 15:00:03 -0500

    Able to read ASPEX Spectrum TIFF files.

--- src/NeXLParticle.jl
+++ src/NeXLParticle.jl
@@ -12,4 +12,7 @@ export elements # An ordered list of elements in quant table
 export header # A alphabetically sorted list of header items
 export data # Provides access to the DataFrame containing the particle data
 
+include("aspextiff.jl")
+# Exposes FileIO.load and FileIO.save for the format"ASPEX TIFF"
+
 end
